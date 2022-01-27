ef6140978b95bf0ae65ba1a1c79121f8c41bac77
Felipe Policisse <policisse@gmail.com>
2022-01-27 20:40:11 -0300

    - Impl Main Screen - Impl client details ( passing values in navigation working ) --> massive changes to impl use_cases - Impl login logic

--- app/src/main/java/com/poli/prevendasomie/data/remote/BackEndApi.kt
+++ app/src/main/java/com/poli/prevendasomie/data/remote/BackEndApi.kt
@@ -1,9 +1,11 @@
 package com.poli.prevendasomie.data.remote
 
+import com.poli.prevendasomie.login.domain.model.Credentials
+import com.poli.prevendasomie.login.domain.model.LoginResponse
 import retrofit2.http.POST
 
 interface BackEndApi {
 
-    @POST("users/registrations")
-    suspend fun executeLogin(username: String, password: String)
+    @POST("login")
+    suspend fun executeLogin(credentials: Credentials): LoginResponse
 }
