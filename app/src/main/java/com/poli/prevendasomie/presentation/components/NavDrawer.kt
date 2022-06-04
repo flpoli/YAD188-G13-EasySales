ba71b3661de1f7b42dc76212b1ab2ecef9f0fffa
Felipe Policisse <policisse@gmail.com>
2022-06-04 16:06:35 -0300

    Feat [geral]: Implement logout support (#55)

--- app/src/main/java/com/poli/prevendasomie/presentation/components/NavDrawer.kt
+++ app/src/main/java/com/poli/prevendasomie/presentation/components/NavDrawer.kt
@@ -1,6 +1,5 @@
 package com.poli.prevendasomie.presentation.components
 
-import android.view.MenuItem
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,8 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Logout
@@ -21,9 +20,22 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.poli.prevendasomie.domain.repository.Preferences
+import com.poli.prevendasomie.navigation.Screen
+import com.poli.prevendasomie.signup.domain.model.Email
+import com.poli.prevendasomie.signup.domain.model.UserData
+import com.poli.prevendasomie.signup.domain.model.Username
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
-fun NavDrawer(){
+fun NavDrawer(
+    navController: NavHostController,
+    preferences: Preferences,
+    scope: CoroutineScope,
+    scaffoldState: ScaffoldState
+){
 
     val items = listOf(
 
@@ -39,7 +51,35 @@ fun NavDrawer(){
 
     DrawerHeader()
 
-    DrawerBody(items = items, onItemClick = {})
+    DrawerBody(
+        items = items,
+        onItemClick = {
+
+            if(it.id == "1"){
+
+                preferences.isUserLoggedIn(false)
+                preferences.saveUserSession(
+
+                    user = UserData(
+                        Username(""), Email(""), appKey = "", appSecret = ""
+                    )
+                )
+                scope.launch {
+
+                    scaffoldState.drawerState.close()
+                    navController.navigate(Screen.LoginScreen.route)
+                }
+
+
+
+
+            }
+
+        }
+
+
+
+    )
 }
 
 
